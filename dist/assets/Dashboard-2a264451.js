aebf8ed671556fbb13484b7483336e5a05d54bc3
akash@maticui <akash@maticui.com>
2025-01-24 08:52:59 +0530

    socket and frontend update

--- dist/assets/Dashboard-2a264451.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-ccd9abbd.js";const a=()=>r.jsx("div",{children:"Dashboard"});export{a as default};
---
+++ dist/assets/Dashboard-f1943b80.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-72f554a9.js";const a=()=>r.jsx("div",{children:"Dashboard"});export{a as default};
